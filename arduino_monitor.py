a607272f306662f53d4658aa88a8bfe590c62d54
crhntr <hunter.christopher@icloud.com>
2018-02-15 20:17:25 -0800

    Added --serial Flag

--- arduino_monitor.py
+++ arduino_monitor.py
@@ -3,9 +3,15 @@
 import serial
 import os
 from datetime import datetime
+import argparse
 
 # Setup
-ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
+parser = argparse.ArgumentParser(description='Process data from serial port sent from the arduino.')
+parser.add_argument('--serial', dest='serial_name', action='store_const',
+                    default='/dev/ttyUSB0',
+                    help='serial port where arduino data can be read from. On *nix it is found in ')
+
+ser = serial.Serial(parser.serial_name, baudrate=9600, timeout=1)
 
 # Loop
 while 1:
