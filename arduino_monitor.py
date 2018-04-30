d71cdb9c29cb696b4918fcb024e78e73e6da5e01
crhntr <hunter.christopher@icloud.com>
2018-04-29 22:11:27 -0700

    Minor Change

--- arduino_monitor.py
+++ arduino_monitor.py
@@ -7,9 +7,7 @@ import argparse
 
 # Setup
 parser = argparse.ArgumentParser(description='Process data from serial port sent from the arduino.')
-parser.add_argument('--serial', dest='serial_name', action='store_const',
-                    default='/dev/ttyUSB0',
-                    help='serial port where arduino data can be read from. On *nix it is found in ')
+parser.add_argument('--serial', dest='serial_name', action='store_const', default='/dev/ttyUSB0', help='serial port where arduino data can be read from. On *nix it is found in ')
 
 ser = serial.Serial(parser.serial_name, baudrate=9600, timeout=1)
 
